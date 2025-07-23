9d4964c5d15d813c13ccc24ee296230ea946205f
Muhammad Ali <saeedtayyab248@gmail.com>
2025-07-23 19:35:52 +0500

    Update app4.js

--- app4.js
+++ app4.js
@@ -8,6 +8,6 @@ for(let i = 1; i <= 100; i++) {
     }
 }
 
-document.writeln("Sum of multiples of 5 between 1 and 100 is: " + sum);
+console.log("Sum of multiples of 5 between 1 and 100 is: " + sum);
 }
-sum()
+
