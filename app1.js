f387753d57a10f041e908d9d626e3e3dc67e6947
Muhammad Ali <saeedtayyab248@gmail.com>
2025-07-23 20:14:15 +0500

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,6 +1,6 @@
 function rgbColors() {
 for(var i=0; i<1; i++ ){
-    for(var j=0; j<10; j++ ){
+    for(var j=0; j<1; j++ ){
     var r = Math.floor(Math.random() * 256);
     var g = Math.floor(Math.random() * 256);
     var b = Math.floor(Math.random() * 256);
