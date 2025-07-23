99150b7503c044a81c0aeb6ae7f282c175183842
Muhammad Ali <saeedtayyab248@gmail.com>
2025-07-23 19:35:17 +0500

    Update app3.js

--- app3.js
+++ app3.js
@@ -5,20 +5,19 @@ for(var i=1; i <= 100; i++){
 
         if(i % 5 == 0 && i % 3 == 0 ){
         text = "FizzBuzz<br>"
-        document.write(text)
+        console.log(text)
     }
         else if(i % 3 == 0){
         text = "Fizz<br>"
-        document.write(text)
+        console.log(text)
     }
     else if(i % 5 == 0 && i % 3 !== 0 ){
         text = "Buzz<br>"
-        document.write(text)
+        console.log(text)
     }
     else{
         text = "Not Divisible by 3 or 5<br>"
-        document.write(text)
+        console.log(text)
     }
 }
 }
-fizzbuzz()
